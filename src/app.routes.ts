6a5bbac09258d1e054521a3da153858dad23628c
Bryhan <bryhanstif@gmail.com.com>
2025-04-08 16:10:34 -0500

    cambio en caliente primera revision por la alcaldia

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -1,30 +1,32 @@
 import { Routes } from '@angular/router';
-import { ListRespComponent } from './shared/components/list-resp/list-resp.component';
-import { authGuard } from './app/guards/auth.guard';
+// import { ListRespComponent } from './shared/components/list-resp/list-resp.component';
+// import { authGuard } from './app/guards/auth.guard';
 
 export const appRoutes: Routes = [
-    { path: '', loadComponent: () => import('./app/pages/home/home.component'), },
-    {
-        path: 'dashboard',
-        loadComponent: () => import('./app/layout/component/app.layout'),
-        canActivate: [authGuard],
-        children: [
-            { path: '', loadComponent: () => import('./app/dashboard/dashboard') },
-            { path: 'apps', loadChildren: () => import('./app/apps/apps.routes') },
-            { path: 'uikit', loadChildren: () => import('./app/pages/uikit/uikit.routes') },
-            { path: 'documentation', loadComponent: () => import('./app/documentation/documentation.component') },
-            { path: 'list', component: ListRespComponent },
-        ]
-    },
-    { path: '',
-        children: [
-            { path: 'auth', loadChildren: () => import('./app/pages/auth/auth.routes') },
-            { path: 'pages', loadChildren: () => import('./app/pages/pages.routes')},
-            { path: 'landing', loadComponent: () => import('./app/pages/landing/landing.component')},
-            { path: 'landing2', loadComponent: () => import('./app/pages/landing2/landing.component')},
+    { path: "", loadComponent: () => import('../src/app/pages/questions/questions.component') },
+    // { path: '', loadComponent: () => import('./app/pages/home/home.component'), },
+
+    // {
+    //     path: 'dashboard',
+    //     loadComponent: () => import('./app/layout/component/app.layout'),
+    //     canActivate: [authGuard],
+    //     children: [
+    //         { path: '', loadComponent: () => import('./app/dashboard/dashboard') },
+    //         { path: 'apps', loadChildren: () => import('./app/apps/apps.routes') },
+    //         { path: 'uikit', loadChildren: () => import('./app/pages/uikit/uikit.routes') },
+    //         { path: 'documentation', loadComponent: () => import('./app/documentation/documentation.component') },
+    //         { path: 'list', component: ListRespComponent },
+    //     ]
+    // },
+    // { path: '',
+    //     children: [
+    //         { path: 'auth', loadChildren: () => import('./app/pages/auth/auth.routes') },
+    //         { path: 'pages', loadChildren: () => import('./app/pages/pages.routes')},
+    //         { path: 'landing', loadComponent: () => import('./app/pages/landing/landing.component')},
+    //         { path: 'landing2', loadComponent: () => import('./app/pages/landing2/landing.component')},
             { path: 'notfound', loadComponent: () => import('./app/pages/notfound/notfound.component')},
-        ]
-    },
+    //     ]
+    // },
 
     { path: '**', redirectTo: '/notfound' }
 ];
--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import { HighlightsWidget } from './landing2/components/highlightswidget/highlightswidget.component';
+// import { HighlightsWidget } from './landing2/components/highlightswidget/highlightswidget.component';
 export default [
-    { path: "home", loadComponent: () => import('./home/home.component')},
+    // { path: "home", loadComponent: () => import('./home/home.component')},
     { path: "questions", loadComponent: () => import('./questions/questions.component')},
     { path: '**', redirectTo: '/notfound' },
-    { path: 'landing2', component: HighlightsWidget },
+    // { path: 'landing2', component: HighlightsWidget },
 ] as Routes;
