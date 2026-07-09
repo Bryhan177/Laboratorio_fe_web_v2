d4e2ab78e0e5b7c2c56e1b3abc537e1caf757fa7
AlexisHz2023 <alexisdurango721@gmail.com>
2026-07-09 13:16:06 -0500

    update web branch

--- src/app/pages/landing2/components/herowidget/herowidget.component.ts
+++ src/app/pages/landing2/components/herowidget/herowidget.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { AnimateOnScrollModule } from 'primeng/animateonscroll';
 import { ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
 
 @Component({
     selector: 'hero-widget',
-    imports: [ButtonModule, RippleModule],
+    imports: [ButtonModule, RippleModule, AnimateOnScrollModule],
   templateUrl: './herowidget.component.html',
   styleUrl: './herowidget.component.scss'
 })
