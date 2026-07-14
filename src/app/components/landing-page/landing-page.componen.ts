6d754e9f3341039ddf9426cfc85cc01779b5e10c
AlexisHz2023 <alexisdurango721@gmail.com>
2026-07-14 18:37:28 -0500

    update web 1.0

--- src/app/components/landing-page/landing-page.componen.ts
+++ src/app/components/landing-page/landing-page.componen.ts
@@ -6,20 +6,24 @@ import { TeamSectionComponent } from '../team-section/team-section.componen';
 import { NavigationMenuComponent } from '../navigation-menu/navigation-menu.component';
 import { FooterComponent } from '../footer/footer.component';
 import { WelcomeOverlayComponent } from '../welcome-overlay/welcome-overlay.component';
+import { CalendarSectionComponent } from '../calendar-section/calendar.component';
 import { VisitorContextService } from '../../service/visitor-context.service';
+import { PartTeamComponent } from "../part-team/part-team.component";
 
 @Component({
     selector: 'app-landing-page',
     standalone: true,
     imports: [
-        HeroSectionComponent,
-        ServicesSectionComponent,
-        TeamSectionComponent,
-        StatsSectionComponent,
-        NavigationMenuComponent,
-        FooterComponent,
-        WelcomeOverlayComponent
-    ],
+    HeroSectionComponent,
+    ServicesSectionComponent,
+    TeamSectionComponent,
+    StatsSectionComponent,
+    NavigationMenuComponent,
+    FooterComponent,
+    WelcomeOverlayComponent,
+    CalendarSectionComponent,
+    PartTeamComponent
+],
     templateUrl: './landing-page.component.html'
 })
 export default class LandingPageComponent implements OnInit {
